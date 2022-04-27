15b186a9cd66c2bedf57f4d01cac6c78a0b9a8ef
mallocreal <76497029+mallocreal@users.noreply.github.com>
2022-04-27 23:38:05 +0100

    message

--- app/src/main/java/com/example/button2/MainActivity.java
+++ app/src/main/java/com/example/button2/MainActivity.java
@@ -10,32 +10,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+//  say my name ok see you
 
+public class MainActivity extends AppCompatActivity {
 
-    public class MainActivity extends AppCompatActivity {
+    Button simpleButton1, simpleButton2;
 
-        Button simpleButton1, simpleButton2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleButton1 = (Button) findViewById(R.id.simpleButton1);// get id of button 1
+        simpleButton2 = (Button) findViewById(R.id.simpleButton2);// get id of button 2
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            simpleButton1 = (Button) findViewById(R.id.simpleButton1);//get id of button 1
-            simpleButton2 = (Button) findViewById(R.id.simpleButton2);//get id of button 2
-
-            simpleButton1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(getApplicationContext(), "Simple Button 1", Toast.LENGTH_LONG).show();//display the text of button1
-                }
-            });
-            simpleButton2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(getApplicationContext(), "Simple Button 2", Toast.LENGTH_LONG).show();//display the text of button2
-                }
-            });
-        }
-
+        simpleButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Simple Button 1", Toast.LENGTH_LONG).show();// display the text
+                                                                                                     // of button1
+            }
+        });
+        simpleButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Simple Button 2", Toast.LENGTH_LONG).show();// display the text
+                                                                                                     // of button2
+            }
+        });
+    }
 
 }
